723979bf12e5542244444a18445cad2a498f30ba
CREATE-04 <97846880+RoyalT-Wrecks@users.noreply.github.com>
2023-10-24 19:55:56 -0500

    Added xbox controller and teleop driving

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -8,6 +8,7 @@ package frc.robot;
 import edu.wpi.first.wpilibj.TimedRobot;
 import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+import edu.wpi.first.wpilibj.XboxController;
 
 /**
  * The VM is configured to automatically run this class, and to call the functions corresponding to
@@ -22,6 +23,7 @@ public class Robot extends TimedRobot {
   private final SendableChooser<String> m_chooser = new SendableChooser<>();
 
   private final RomiDrivetrain m_drivetrain = new RomiDrivetrain();
+  private XboxController m_controller = new XboxController(Constants.InputDevices.XBOX_CONTROLLER);
 
   /**
    * This function is run when the robot is first started up and should be used for any
@@ -83,7 +85,9 @@ public class Robot extends TimedRobot {
 
   /** This function is called periodically during operator control. */
   @Override
-  public void teleopPeriodic() {}
+  public void teleopPeriodic() {
+    m_drivetrain.arcadeDrive(m_controller.getLeftY(), m_controller.getRightX());
+  }
 
   /** This function is called once when the robot is disabled. */
   @Override

da7474a8981632e9895cf9a20215eda7274cf0bb
Sarah Huang <sarahjhuang08@icloud.com>
2023-10-24 19:51:16 -0500

    Add control function

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -7,6 +7,7 @@ package frc.robot;
 import edu.wpi.first.wpilibj.TimedRobot;
 import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+import edu.wpi.first.wpilibj.XboxController;
 
 /**
  * The VM is configured to automatically run this class, and to call the functions corresponding to
@@ -83,8 +84,10 @@ public class Robot extends TimedRobot {
 
   /** This function is called periodically during operator control. */
   @Override
-  public void teleopPeriodic() {}
-
+  public void teleopPeriodic() {
+    m_drivetrain.arcadeDrive(m_controller.getLeftY(), m_controller.getRightX());
+  }
+// allows you to connect to the robot and manually control it.
   /** This function is called once when the robot is disabled. */
   @Override
   public void disabledInit() {}

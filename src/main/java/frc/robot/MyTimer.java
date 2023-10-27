d6042bee4454acf9782bdb97c8e3bc2343e61143
CREATE-03 <97846880+RoyalT-Wrecks@users.noreply.github.com>
2023-10-26 19:34:22 -0500

    Added a test for changing things in timer class

---
+++ src/main/java/frc/robot/MyTimer.java
@@ -0,0 +1,14 @@
+// package frc.robot;
+
+// import edu.wpi.first.wpilibj.DriverStation;
+// import edu.wpi.first.wpilibj.Timer;
+
+
+// public class MyTimer extends Timer {
+
+    
+//     @Override
+//     public static double getMatchTime() {
+//         return DriverStation.getMatchTime();
+//     }
+// }

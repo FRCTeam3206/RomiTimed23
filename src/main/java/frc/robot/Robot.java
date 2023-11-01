a2f0d3da9e067cb01419aaf5cd60f16480d85df0
CREATE-03 <97846880+RoyalT-Wrecks@users.noreply.github.com>
2023-10-31 19:54:41 -0500

    slow modes working, go to angle option in progress

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -25,7 +25,19 @@ public class Robot extends TimedRobot {
   private final RomiDrivetrain m_drivetrain = new RomiDrivetrain();
   private XboxController m_controller = new XboxController(Constants.InputDevices.XBOX_CONTROLLER);
 
-  private String mode;
+  private double speed;
+  private double rotate;
+  private boolean slowerBackwards = true;
+  private boolean slowMode = false;
+
+  public static final double distWheelsInch = 5.487402;
+  private boolean turnGoalSet = false;
+  private int turnAngle;
+  private double turnDistance;
+  private double turnGoal;
+
+  private double leftEncoderOffset = 0;
+  private double rightEncoderOffset = 0;
 
   /**
    * This function is run when the robot is first started up and should be used for any
@@ -47,8 +59,8 @@ public class Robot extends TimedRobot {
    */
   @Override
   public void robotPeriodic() {
-    SmartDashboard.putNumber("Left Distance (in)", m_drivetrain.getLeftDistanceInch());
-    SmartDashboard.putNumber("Right Distance (in)", m_drivetrain.getRightDistanceInch());
+    SmartDashboard.putNumber("Left Distance (in)", m_drivetrain.getLeftDistanceInch() - leftEncoderOffset);
+    SmartDashboard.putNumber("Right Distance (in)", m_drivetrain.getRightDistanceInch() - rightEncoderOffset);
   }
   /**
    * This autonomous (along with the chooser code above) shows how to select between different
@@ -86,28 +98,80 @@ public class Robot extends TimedRobot {
   /** This function is called once when teleop is enabled. */
   @Override
   public void teleopInit() {
-    mode = "normal";
-  }
+ }
 
   /** This function is called periodically during operator control. */
   @Override
   public void teleopPeriodic() {
-    // Changes mode based on button pressed (Y for slow mode and A for normal)
-    if (m_controller.getYButton() == true) {
-      mode = "slow";
-    } else if (m_controller.getAButton() == true) {
-      mode = "normal";
+    if (m_controller.getStartButton()) {
+      leftEncoderOffset = m_drivetrain.getLeftDistanceInch();
+      rightEncoderOffset = m_drivetrain.getRightDistanceInch();
     }
-    // Drives based on mode
-    if (mode == "normal") {
-      m_drivetrain.arcadeDrive(-m_controller.getLeftY(), 0.7*m_controller.getRightX());
-    } else if (mode == "slow") {
-      m_drivetrain.arcadeDrive(-0.5*m_controller.getLeftY(), 0.7*m_controller.getRightX());
+    speed = -0.8 * m_controller.getLeftY();
+    rotate = 0.7 * m_controller.getRightX();
+    // b deactivates slower backwards, x activates it, does slower backward if slowerBackwards is true
+    if (m_controller.getBButton()) {
+      slowerBackwards = false;
+    } else if (m_controller.getXButton()) {
+      slowerBackwards = true;
     }
-    // Resets encoders when B button pressed
-    if (m_controller.getBButtonPressed() == true) {
-      m_drivetrain.resetEncoders();
+    if (slowerBackwards && speed < 0) {
+      speed *= 0.7;
     }
+    // y activates slow mode, a deactivates slow mode, does slow mode if slowMode is true
+    if (m_controller.getYButton()) {
+      slowMode = true;
+    } else if (m_controller.getAButton()) {
+      slowMode = false;
+    }
+    if (slowMode) {
+      speed *= 0.7;
+    }
+    // setup for going to angle
+    if (m_controller.getPOV() != -1) {
+      turnGoalSet = true;
+      if (m_controller.getPOV() > 180) {
+        turnAngle = m_controller.getPOV() - 360;
+      } else {
+        turnAngle = m_controller.getPOV();
+      }
+      turnDistance = distWheelsInch * Math.PI * (turnAngle / 180);
+      turnGoal = m_drivetrain.getLeftDistanceInch() - m_drivetrain.getRightDistanceInch() + turnDistance;
+    }
+    // change rotation to go to angle
+    if (turnGoalSet) {
+      if (turnGoal > 0) { // for turning right or 180
+        if ((m_drivetrain.getLeftDistanceInch() - m_drivetrain.getRightDistanceInch()) < turnGoal) { // goal hasn't been reached yet
+          rotate = 0.7 * ((turnGoal - ((m_drivetrain.getLeftDistanceInch() - m_drivetrain.getRightDistanceInch()))) / (distWheelsInch * Math.PI / (turnAngle / 360))) + 0.3;
+        } else { // goal has been reached
+          turnGoalSet = false;
+        }
+      } else { // for turning left
+        if ((m_drivetrain.getLeftDistanceInch() - m_drivetrain.getRightDistanceInch()) > turnGoal) { // goal hasn't been reached yet
+          rotate = -0.7 * ((turnGoal - (m_drivetrain.getLeftDistanceInch() - m_drivetrain.getRightDistanceInch())) / (distWheelsInch * Math.PI / (-turnAngle / 360))) + 0.3;
+        } else { // goal has been reached
+          turnGoalSet = false;
+        }
+      }
+    }
+    m_drivetrain.arcadeDrive(speed, rotate);
+
+    // // Changes mode based on button pressed (Y for slow mode and A for normal)
+    // if (m_controller.getYButton() == true) {
+    //   mode = "slow";
+    // } else if (m_controller.getAButton() == true) {
+    //   mode = "normal";
+    // }
+    // // Drives based on mode
+    // if (mode == "normal") {
+    //   m_drivetrain.arcadeDrive(-m_controller.getLeftY(), 0.7*m_controller.getRightX());
+    // } else if (mode == "slow") {
+    //   m_drivetrain.arcadeDrive(-0.5*m_controller.getLeftY(), 0.7*m_controller.getRightX());
+    // }
+    // // Resets encoders when B button pressed
+    // if (m_controller.getBButtonPressed() == true) {
+    //   m_drivetrain.resetEncoders();
+    // }
   }
 
   /** This function is called once when the robot is disabled. */

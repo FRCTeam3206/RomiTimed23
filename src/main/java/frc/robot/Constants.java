f966a6b22a031e957624d536092e39436368be8a
CREATE-03 <97846880+RoyalT-Wrecks@users.noreply.github.com>
2023-11-21 19:28:09 -0600

    Button constants (still need to implement)

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -4,4 +4,11 @@ public class Constants {
     public static class InputDevices {
         public static final int XBOX_CONTROLLER = 0;
     }
+    public static class Buttons {
+        public static final int RESET_TELEOP_ENCODERS = 0;
+        public static final int DEACTIVATE_SLOWER_BACKWARDS = 0;
+        public static final int ACTIVATE_SLOWER_BACKWARDS = 0;
+        public static final int ACTIVATE_SLOW_MODE = 0;
+        public static final int DEACTIVATE_SLOW_MODE = 0;
+    }
 }

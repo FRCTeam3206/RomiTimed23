c0fcfa53fd158bbc16f1f3cbbca08df918791f81
CREATE-03 <97846880+RoyalT-Wrecks@users.noreply.github.com>
2023-11-21 19:39:07 -0600

    Made button values accurate

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -5,10 +5,10 @@ public class Constants {
         public static final int XBOX_CONTROLLER = 0;
     }
     public static class Buttons {
-        public static final int RESET_TELEOP_ENCODERS = 0;
-        public static final int DEACTIVATE_SLOWER_BACKWARDS = 0;
-        public static final int ACTIVATE_SLOWER_BACKWARDS = 0;
-        public static final int ACTIVATE_SLOW_MODE = 0;
-        public static final int DEACTIVATE_SLOW_MODE = 0;
+        public static final int RESET_TELEOP_ENCODERS = 8; // start
+        public static final int DEACTIVATE_SLOWER_BACKWARDS = 2; // b button
+        public static final int ACTIVATE_SLOWER_BACKWARDS = 3; // x button
+        public static final int ACTIVATE_SLOW_MODE = 4; // y button
+        public static final int DEACTIVATE_SLOW_MODE = 1; // a button
     }
 }

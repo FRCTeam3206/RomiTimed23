7da6e622390792d60785708816df4471107b27fe
CREATE-01 <97846880+RoyalT-Wrecks@users.noreply.github.com>
2023-11-28 20:11:24 -0600

    worked on getting command based set up

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -2,6 +2,7 @@ package frc.robot;
 
 import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
 import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.RunCommand;
 import edu.wpi.first.wpilibj2.command.button.CommandGenericHID;
 import frc.robot.subsystems.RomiDrivetrain;
 
@@ -16,8 +17,9 @@ public class RobotContainer {
         auton();
     }
 
-    private void configureBindings() {
-        // xbox.button(1).whileTrue();
+    private void configureBindin
+
+    drive.setDefaultCommand(new RunCommand(()->drive.arcadeDrive(0.0,0.0),drive));
     }
 
     private void auton() {
--- src/main/java/frc/robot/RomiDrivetrain.java
+++ src/main/java/frc/robot/RomiDrivetrain.java
@@ -2,14 +2,13 @@
 // Open Source Software; you can modify and/or share it under the terms of
 // the WPILib BSD license file in the root directory of this project.
 
-package frc.robot.subsystems;
+package frc.robot;
 
 import edu.wpi.first.wpilibj.Encoder;
 import edu.wpi.first.wpilibj.drive.DifferentialDrive;
 import edu.wpi.first.wpilibj.motorcontrol.Spark;
-import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
-public class RomiDrivetrain extends SubsystemBase {
+public class RomiDrivetrain {
   private static final double kCountsPerRevolution = 1440.0;
   private static final double kWheelDiameterInch = 2.75591; // 70 mm
 
